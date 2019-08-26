338fc8860c0485e88c0ab3026c050018ae24f522
Lohitaksh Trehan <lohitaksh.trehan@iongroup.com>
2019-08-26 14:35:43 +0530

    Js addition started, starting by signUp feature, old JS files mostly phased out
    Signed-off-by: Lohitaksh Trehan <lohitaksh.trehan@iongroup.com>

--- public/scripts/signup.js
+++ public/scripts/signup.js
@@ -1,59 +1,48 @@
-import validateData from "./validate.js"
-export default signUp;
-function signUp() {
-    if(document.getElementById("loginDiv")!==null)
-        document.getElementById("loginDiv").remove();
-    if(document.getElementById("main-content")!==null)
-        document.getElementById("main-content").remove();
-    if(document.getElementById("homepage_nav")!==null)
-        document.getElementById("homepage_nav").remove();
-    document.getElementById("bodyTag").setAttribute("style","");
+window.onload = function(){
+    document.getElementById("RegisterPerson").addEventListener('click',function(event){
+        event.preventDefault();
+        //start registering the person and look for inline error marking
+        var isValid = validate();
+    });
+    console.log("signUp.js called");
+}
 
-    var mainContentDiv = document.createElement("div");
-    mainContentDiv.setAttribute("class","container-fluid");
-    mainContentDiv.setAttribute("id","signup-div");
-    document.getElementById("bodyTag").prepend(mainContentDiv);
-    var x = `
-            <div class="d-flex justify-content-center">
-                <div class="card">
-                    <div class="form-group">
-                        <label for="Name">Name:</label>
-                        <input type="text" class="form-control" id="Name" placeholder="Type your name here">
-                    </div>
-                    <div class="form-group">
-                        <label for="BirthDate">Birth Date:</label>
-                        <input type="date" class="form-control" id="BirthDate" placeholder="Type your Birth Date here">
-                    </div>
-                    <div class="form-group">
-                        <label for="empId">Employee Id:</label>
-                        <input type="text" class="form-control" id="empId" placeholder="Type Employee Id here">
-                    </div>
-                    <div class="form-group">
-                        <label for="Email">Email:</label>
-                        <input type="text" class="form-control" id="Email" placeholder="Type your email here">
-                    </div>
-                    <div class="form-group">
-                        <label for="pass">Password(atleast 3 characters):</label>
-                        <input type="password" class="form-control" id="pass" placeholder="Type your password here">
-                    </div>
-                    <button id="RegisterPerson" type="submit" class="btn btn-primary">SignUp</button>
-                </form>
-            </div>
-    `
-    mainContentDiv.innerHTML = x;
+function validate(){
+    var letters = /^[A-Za-z]+$/;
+    var email = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var empIdPattern = /^[a-zA-Z]\-[0-9]+$/;
+    var dob = document.getElementById("BirthDate").value; //empId, pass
+    console.log(dob);
+    var name = document.getElementById("Name").value.trim();
+    var nameArr = name.split(" ");
+    var enteredEmail = document.getElementById("Email").value;
+    var empId = document.getElementById("empId").value;
+    var pass = document.getElementById("pass").value;
+    var confirm_pass = document.getElementById("confirm_pass").value;
 
-    var navBar = document.createElement("div");
-    navBar.setAttribute("id","signup-nav");
-    navBar.setAttribute("class","container-fluid");
-    navBar.innerHTML = `
-                        <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-                            <a class="navbar-brand" href="#" onClick="document.location.reload(true)">
-                                <img src="./assets/ION_logo.png" width="50" height="30" alt="">
-                            </a>
-                            <a class="navbar-brand" href="#" onClick="document.location.reload(true)">Birthday Notifier</a>
-                        </nav>
-    `;
-    document.getElementById("bodyTag").prepend(navBar);
+    var err = "";
+    for(let i=0;i<nameArr.length;i++){
+        if(nameArr[i].match(letters)===null){
+            err+="Name field contains more than one white space together, or something else than alphabets\n";
+            break;
+        }
+    }
 
-    document.getElementById("RegisterPerson").addEventListener('click',function(event){validateData(event);});
+    if(enteredEmail.match(email)===null){
+        err+="format of entered email is incorrect \n"
+    }
+
+    if(empId.match(empIdPattern)===null){
+        err+="incorrect format of empId\n";
+    }
+
+    if(pass.length<3){
+        err+="password length too short\n";
+    }
+
+    if(pass!==confirm_pass){
+        err+="the passwords dont match"
+    }
+    
+    console.log(err)
 }
